862c545737074484f25b78574c5afb0ebc71bf2f
Alex Luong <alex@alexluong.com>
2022-03-13 14:38:42 +0700

    Add search by Twitter username feature

--- components/AppHero.js
+++ components/AppHero.js
@@ -1,4 +1,4 @@
-export default function AppHero() {
+export function AppHero({ input }) {
   return (
     <section className="">
       <div className="container px-5 py-24 mx-auto ">
@@ -15,31 +15,7 @@ export default function AppHero() {
           </p>
         </div>
         <div className="flex flex-col sm:flex-row lg:w-2/3 w-full mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-          <div className="inline-flex w-full rounded border border-gray-300">
-            <input
-              ype="text"
-              id="space-search"
-              name="space-search"
-              placeholder="Search for a space"
-              className="w-full bg-slate-100 bg-opacity-50  focus:border-twitterblue focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-            <div className="w-1/12 pt-2 bg-twitterblue rounded border border-gray-300 ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 mx-auto text-white"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </div>
-          </div>
+          {input}
         </div>
         <div>
           <div
